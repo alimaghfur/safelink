a5d1378c2be6919512ec6ee8cfc80eb447e54f2d
alimaghfur <35333641+alimaghfur@users.noreply.github.com>
2018-08-09 08:35:01 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="learning-to-drive-and-costs.html"}
 if(x=="2"){xxx="is-dog-insurance-really-worth-it.html"}
 if(x=="3"){xxx="know-several-types-of-insurance.html"}
-if(x=="4"){xxx="quick-guide-to-insurance-claim.html"}a_to_ve[i].href="http://safelinkurls.blogspot.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="comparing-car-insurance-policies-is.html"}a_to_ve[i].href="http://safelinkurls.blogspot.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
